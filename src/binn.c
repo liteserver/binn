43a61b64939e0b247b08de31dda9544a68bc9ae2
Bernardo Ramos <berna.gensis@gmail.com>
2020-05-03 23:00:17 -0300

    use snprintf to compile on VS

--- src/binn.c
+++ src/binn.c
@@ -3418,7 +3418,7 @@ char * APIENTRY binn_get_str(binn *value) {
 
   if (type_family(value->type) == BINN_FAMILY_INT) {
     if (copy_int_value(value->ptr, &vint, value->type, BINN_INT64) == FALSE) return NULL;
-    sprintf(buf, "%" INT64_FORMAT, vint);
+    snprintf(buf, sizeof buf, "%" INT64_FORMAT, vint);
     goto loc_convert_value;
   }
 
@@ -3426,7 +3426,7 @@ char * APIENTRY binn_get_str(binn *value) {
   case BINN_FLOAT:
     value->vdouble = value->vfloat;
   case BINN_DOUBLE:
-    sprintf(buf, "%g", value->vdouble);
+    snprintf(buf, sizeof buf, "%g", value->vdouble);
     goto loc_convert_value;
   case BINN_STRING:
     return (char*) value->ptr;
